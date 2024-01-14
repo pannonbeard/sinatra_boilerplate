6a9e27af4132946030d48e435529311bfe9588e7
Phil Hendricks <pfhendri1@gmail.com>
2024-01-14 12:44:03 -0600

    added Application Record for base of models

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,9 @@ config_path = File.join(__dir__, 'database.yml')
 ActiveRecord::Base.configurations = YAML.load_file(config_path)
 ActiveRecord::Base.establish_connection(:development)
 
-require File.expand_path(File.join('app', 'helpers', 'application_helper.rb'))
-require File.expand_path(File.join('app', 'controllers', 'application_controller.rb'))
+require File.expand_path(File.join('app', 'helpers', 'application_helper'))
+require File.expand_path(File.join('app', 'controllers', 'application_controller'))
+require File.expand_path(File.join('app', 'models', 'application_record'))
 
 Dir[File.join('app','helpers', '**/*._helper.rb')].each { |file| require File.expand_path(file) }
 Dir[File.join('app', 'controllers', '**/*_controller.rb')].each { |file| require File.expand_path(file) }
